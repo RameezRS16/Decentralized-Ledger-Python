e5323e1d7624b64b1b6f56138a57d7da0f6fd1c9
Rameez Raja Soomro <rameezrajasoomro83@gmail.com>
2025-03-28 01:50:03 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,28 +1,10 @@
-import hashlib
-import json
+from utils import calculate_hash
 
-DIFFICULTY = 4  # Number of leading zeros required in hash
-
-def proof_of_work(block):
-    block.nonce = 0
-    computed_hash = calculate_hash(block)
-    
-    while not computed_hash.startswith('0' * DIFFICULTY):
-        block.nonce += 1
-        computed_hash = calculate_hash(block)
-    
-    return computed_hash
-
-def calculate_hash(block):
-    block_string = json.dumps({
-        "index": block.index,
-        "timestamp": block.timestamp,
-        "data": block.data,
-        "previous_hash": block.previous_hash,
-        "nonce": block.nonce
-    }, sort_keys=True)
-    
-    return hashlib.sha256(block_string.encode()).hexdigest()
-
-def is_valid_transaction(data):
-    return isinstance(data, str) and len(data) > 0
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash, nonce=0):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = calculate_hash(self)
